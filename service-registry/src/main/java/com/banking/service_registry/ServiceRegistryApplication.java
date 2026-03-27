02cfb96ae9b90c55b47ab28273c2f3cd3c2dff27
Shuubham53 <shubhamnishad110@gmail.com>
2026-03-27 17:31:18 +0530

    service-registry running on port 8761

--- service-registry/src/main/java/com/banking/service_registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/banking/service_registry/ServiceRegistryApplication.java
@@ -2,7 +2,10 @@ package com.banking.service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
