26ec5e870b0cce097c3ed6a13c275c89079cd420
Shuubham53 <shubhamnishad110@gmail.com>
2026-04-05 18:59:47 +0530

    feat: account-service complete - entity, service, controller, Feign client

---
+++ account-service/src/main/java/com/banking/account_service/security/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package com.banking.account_service.security;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException, java.io.IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        if (jwtUtil.validateToken(token) &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            String username = jwtUtil.extractUsername(token);
+
+            // no DB call needed — build authentication directly from token
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(
+                            username, null,
+                            List.of(new SimpleGrantedAuthority("ROLE_CUSTOMER"))
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+            log.info("User authenticated from token: {}", username);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
