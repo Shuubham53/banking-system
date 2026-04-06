bd2851a3ed9470abbd72eb5c9aa123afd9294ccd
Shuubham53 <shubhamnishad110@gmail.com>
2026-04-06 18:22:48 +0530

    feat: add entity, enums, repository, DTOs, security, exceptions for transaction-service

---
+++ transaction-service/src/main/java/com/banking/transactionservice/error/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.banking.transactionservice.error;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.auth.InvalidCredentialsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ApiResponse<Void>> handleInsufficientBalanceException(
+            InsufficientBalanceException ex) {
+        log.error(" Insufficient balance {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidTransactionException.class)
+    public ResponseEntity<ApiResponse<Void>> handleInvalidTransactionException(
+            InvalidTransactionException ex) {
+        log.error("Invalid Transaction: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+    @ExceptionHandler(TransactionNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleTransactionNotFoundException(
+            TransactionNotFoundException ex) {
+        log.error("Transaction not found: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleGenericException(
+            Exception ex) {
+        log.error("Unexpected error: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("Something went wrong"));
+    }
+}
