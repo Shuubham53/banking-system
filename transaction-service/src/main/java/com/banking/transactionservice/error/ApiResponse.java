824d852070697064a782f1038d2c37d24c0ebc75
Shuubham53 <shubhamnishad110@gmail.com>
2026-04-10 15:26:03 +0530

    feat: add TransactionController, FeignInterceptor fixes

--- transaction-service/src/main/java/com/banking/transactionservice/error/ApiResponse.java
+++ transaction-service/src/main/java/com/banking/transactionservice/error/ApiResponse.java
@@ -1,13 +1,19 @@
 package com.banking.transactionservice.error;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
 public class ApiResponse<T>{
     private boolean success;
     private String message;
